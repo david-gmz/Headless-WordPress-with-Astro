918f69deef69610813d517130bdb04941a00c08b
David Gómez <david.gmz.b@gmail.com>
2024-12-14 05:54:24 -0300

    Adding postByTagQuery feature

---
+++ src/lib/postByTagQuery.ts
@@ -0,0 +1,30 @@
+import { wpQuery } from "./wpQuery";
+
+export const postByTagQuery = async tag => {
+    const data = await wpQuery({
+        query: `
+        query SingleTag($id: ID = "${tag}") {
+          tag(idType: SLUG, id: $id) {
+            posts(where: {orderby: {field: DATE, order: DESC}}) {
+              nodes {
+                date
+                content
+                title
+                slug
+                excerpt
+                featuredImage {
+                  node {
+                    sourceUrl
+                    altText
+                  }
+                }
+              }
+            }
+          }
+        }
+        `
+    });
+
+    //  assign the post info to singlePost variable for usability
+    return data.tag.posts.nodes;
+};
