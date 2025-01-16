edce64f0529726a18a20d2a3a27370a4de80c997
David Gómez <david.gmz.b@gmail.com>
2025-01-16 09:36:44 -0300

    Fix toggle issus, change hero and some style

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,58 @@ export default {
             }
         },
         extend: {
+            animation: {
+                "fade-in": "fade-in 1s ease-in",
+                "slide-in": "slide-in 1s ease-out",
+                "bounce-slow": "bounce 3s infinite"
+            },
+            keyframes: {
+                "fade-in": {
+                    "0%": { opacity: "0" },
+                    "100%": { opacity: "1" }
+                },
+                "slide-in": {
+                    "0%": { transform: "translateX(-100%)" },
+                    "100%": { transform: "translateX(0)" }
+                }
+            },
+            backgroundImage: {
+                "gradient-custom":
+                    "linear-gradient(to bottom right, var(--gradient-start), var(--gradient-middle), var(--gradient-end))",
+                "text-gradient-light":
+                    "linear-gradient(to right, var(--tw-gradient-from) 0%, var(--tw-gradient-to) 100%)",
+                "text-gradient-dark":
+                    "linear-gradient(to right, var(--tw-gradient-from) 0%, var(--tw-gradient-to) 100%)"
+            },
+            gradientColorStops: {
+                heading: {
+                    light: {
+                        from: "#1e293b", // slate-800
+                        via: "#2563eb", // sky-500
+                        to: "#6ee7b7" // emerald-300
+                    },
+                    dark: {
+                        from: "#d9f99d", // lime-200
+                        via: "#eab308", // yellow-500
+                        to: "#a855f7" // purple-500
+                    }
+                }
+            },
             colors: {
+                gradient: {
+                    start: {
+                        DEFAULT: "#ffffff",
+                        dark: "#001407"
+                    },
+                    middle: {
+                        DEFAULT: "#fcedbd",
+                        dark: "#111111"
+                    },
+                    end: {
+                        DEFAULT: "#e5f7ff",
+                        dark: "#000a08"
+                    }
+                },
                 border: "var(--border)",
                 input: "var(--input)",
                 ring: "var(--ring)",
@@ -54,6 +105,40 @@ export default {
             }
         }
     },
-    plugins: [typography]
+    plugins: [
+        typography,
+        require("tailwindcss-motion"),
+        function ({ addComponents, theme }) {
+            addComponents({
+                ".gradient-heading": {
+                    backgroundClip: "text",
+                    WebkitBackgroundClip: "text",
+                    color: "transparent",
+                    backgroundImage:
+                        "linear-gradient(to right, var(--tw-gradient-from), var(--tw-gradient-via), var(--tw-gradient-to))",
+                    "--tw-gradient-from": theme(
+                        "gradientColorStops.heading.light.from"
+                    ),
+                    "--tw-gradient-via": theme(
+                        "gradientColorStops.heading.light.via"
+                    ),
+                    "--tw-gradient-to": theme(
+                        "gradientColorStops.heading.light.to"
+                    )
+                },
+                ".dark .gradient-heading": {
+                    "--tw-gradient-from": theme(
+                        "gradientColorStops.heading.dark.from"
+                    ),
+                    "--tw-gradient-via": theme(
+                        "gradientColorStops.heading.dark.via"
+                    ),
+                    "--tw-gradient-to": theme(
+                        "gradientColorStops.heading.dark.to"
+                    )
+                }
+            });
+        }
+    ]
 };
 
